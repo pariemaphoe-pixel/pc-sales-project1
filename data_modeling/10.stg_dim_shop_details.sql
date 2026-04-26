0829bc20e7e25808d60ae718783d9b1e59ce8165
Parie <pariemaphoe23@gmail.com>
2026-04-27 00:54:42 +0200

    define surrogate keys and load data into dim_shop_details

---
+++ data_modeling/10.stg_dim_shop_details.sql
@@ -0,0 +1,16 @@
+
+---droping table and creating the one with id column
+DROP TABLE [raw_pc_data].[dbo].[dim_shop_details]
+CREATE TABLE [raw_pc_data].[dbo].[dim_shop_details](
+    [Shop_Details_Id] int identity(1,1) primary key,
+	[Shop_Name] [nvarchar](50) NOT NULL,
+	[Shop_Age] [nvarchar](50) NOT NULL
+);
+
+---inseting values in our table from source table
+INSERT INTO [raw_pc_data].[dbo].[dim_shop_details](
+[Shop_Name],[Shop_Age])
+SELECT DISTINCT [Shop_Name],[Shop_Age]
+FROM [raw_pc_data].[dbo].[pc_data];
+
+
