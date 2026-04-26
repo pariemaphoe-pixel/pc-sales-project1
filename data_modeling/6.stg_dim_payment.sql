24cef0bd899fe5f554c15511143ac887b4680430
Parie <pariemaphoe23@gmail.com>
2026-04-27 00:45:35 +0200

    define surrogate keys and load data into dim_payment_method

---
+++ data_modeling/6.stg_dim_payment.sql
@@ -0,0 +1,14 @@
+---dropping theprevious table with no id and creating a new one with an id
+DROP TABLE [raw_pc_data].[dbo].[dim_payment_method]
+CREATE TABLE [raw_pc_data].[dbo].[dim_payment_method](
+    [Payment_Method_id] int identity(1,1) primary key,
+	[Payment_Method] [nvarchar](50) NOT NULL
+) ;
+
+---inserting values from our source table
+INSERT INTO [raw_pc_data].[dbo].[dim_payment_method](
+            [Payment_Method])
+SELECT DISTINCT [Payment_Method]
+FROM [raw_pc_data].[dbo].[pc_data];
+
+
