3a2059b94bcffc5da38540e669d70b016603e0f9
Parie <pariemaphoe23@gmail.com>
2026-04-27 01:30:38 +0200

    implement procedures for automated table initialisation data loading for dim_priority

---
+++ data_modeling/15.usp_stg_dim_priority.sql
@@ -0,0 +1,21 @@
+
+CREATE PROCEDURE dim_stg_priority
+AS
+
+BEGIN
+---droping table with no id and creating a table with id columns
+DROP TABLE [raw_pc_data].[dbo].[dim_priority];
+CREATE TABLE [raw_pc_data].[dbo].[dim_priority](
+    [Priority_Id] int identity(1,1) primary key,
+	[Priority] [nvarchar](50) NOT NULL
+);
+
+---inserting values in our table from the source table
+
+INSERT INTO [raw_pc_data].[dbo].[dim_priority](
+[Priority])
+SELECT DISTINCT [Priority]
+FROM  [raw_pc_data].[dbo].[pc_data]
+END;
+
+
