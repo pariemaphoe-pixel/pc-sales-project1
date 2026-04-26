8207826fee454f8603c613638c8ddb12d2756a6d
Parie <pariemaphoe23@gmail.com>
2026-04-27 01:33:14 +0200

    implement procedures for automated table initialisation data loading for dim_location

---
+++ data_modeling/17.usp_stg_dim_location.sql
@@ -0,0 +1,26 @@
+
+CREATE PROCEDURE dim_stg_location
+AS
+
+BEGIN
+---dropping out previous table with no id and creating a new table with an id
+
+DROP TABLE [raw_pc_data].[dbo].[dim_location]
+CREATE TABLE [raw_pc_data].[dbo].[dim_location](
+    [Location_id] int identity(1,1) primary key,
+	[Continent] [nvarchar](50) NOT NULL,
+	[Country_or_State] [nvarchar](50) NOT NULL,
+	[Province_or_City] [nvarchar](100) NOT NULL
+) ;
+
+---inserting values in our table from our source table
+
+INSERT INTO [raw_pc_data].[dbo].[dim_location](
+[Continent] ,[Country_or_State],
+[Province_or_City])
+SELECT DISTINCT [Continent] ,[Country_or_State],
+[Province_or_City]
+FROM [raw_pc_data].[dbo].[pc_data]
+END;
+
+
