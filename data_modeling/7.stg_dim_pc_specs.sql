0183c9af50276f8ce0f47b902fd30bfa3403b054
Parie <pariemaphoe23@gmail.com>
2026-04-27 00:48:02 +0200

    define surrogate keys and load data into dim_pc_specs

---
+++ data_modeling/7.stg_dim_pc_specs.sql
@@ -0,0 +1,22 @@
+---droping table with no id and creating a table with id column
+
+DROP TABLE [raw_pc_data].[dbo].[dim_pc_specs];
+CREATE TABLE [raw_pc_data].[dbo].[dim_pc_specs](
+    [Pc_Specs_Id] int identity(1,1) primary key, 
+	[Storage_Type] [nvarchar](50) NOT NULL,
+	[RAM] [nvarchar](50) NOT NULL,
+	[Storage_Capacity] [nvarchar](50) NOT NULL,
+	[PC_Make] [nvarchar](50) NOT NULL,
+	[PC_Model] [nvarchar](50) NOT NULL
+) 
+ 
+---inserting values into our table from the source table
+INSERT INTO [raw_pc_data].[dbo].[dim_pc_specs](
+[Storage_Type],[RAM],[Storage_Capacity],
+[PC_Make],[PC_Model])
+SELECT DISTINCT [Storage_Type],[RAM],[Storage_Capacity],
+[PC_Make],[PC_Model]
+FROM [raw_pc_data].[dbo].[pc_data];
+
+
+
