d54e105fef721a1bb6e8b648d202be3075520bc9
Parie <pariemaphoe23@gmail.com>
2026-04-27 01:48:00 +0200

    implement procedures for automated table initialisation data loading for dim_customer_details

---
+++ data_modeling/20.usp_stg_dim_customer_details.sql
@@ -0,0 +1,29 @@
+
+CREATE PROCEDURE dim_stg_customer_details
+AS 
+
+BEGIN
+DROP TABLE [raw_pc_data].[dbo].[dim_customer_details];
+
+CREATE TABLE [raw_pc_data].[dbo].[dim_customer_details](
+    [Customer_Id] int identity(1,1) primary key,
+	[Customer_Name] [nvarchar](50) NOT NULL,
+	[Customer_Surname] [nvarchar](50) NOT NULL,
+	[Customer_Contact_Number] [nvarchar](50) NOT NULL,
+	[Customer_Email_Address] [nvarchar](50) NOT NULL
+) ;
+---SELECT * FROM [raw_pc_data].[dbo].[dim_customer_details]
+INSERT INTO [raw_pc_data].[dbo].[dim_customer_details](
+	[Customer_Name] ,
+	[Customer_Surname],
+	[Customer_Contact_Number] ,
+	[Customer_Email_Address] )
+	SELECT DISTINCT[Customer_Name] ,
+	[Customer_Surname],
+	[Customer_Contact_Number] ,
+	[Customer_Email_Address]
+FROM [raw_pc_data].[dbo].[pc_data]
+END;
+----SELECT * FROM [raw_pc_data].[dbo].[dim_customer_details]
+
+
